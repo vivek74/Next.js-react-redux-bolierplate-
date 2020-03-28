22f220bc24f61a224a48e3a31c30f4e1d4b7b1fe
vivek kumar <vivek.kumar@raweng.com>
2020-03-28 12:15:36 +0530

    'next.js_boilerplate'

---
+++ components/Link/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import styles from './link.module.scss';
+// import './link.scss';
+
+export const Link = ({children}) => {
+    return (
+        <p>{children}</p>
+    )
+}
